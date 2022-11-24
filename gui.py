bdfaf1b60e2e6913f4dc6f139b39b11a3cd0a79c
aprisyazhnoy <113872517+aprisyazhnoy@users.noreply.github.com>
2022-11-24 19:00:09 +0200

    update gui.py
    add file dialog to open txt file as example

--- gui.py
+++ gui.py
@@ -5,14 +5,46 @@ print("module tkinter loaded")
 
 from tkinter import *
 from tkinter import ttk
+from tkinter import filedialog as fd
+from tkinter.messagebox import showinfo
+
+def select_file():
+    filetypes = (
+        ('text files', '*.txt'),
+        ('All files', '*.*')
+    )
+
+    filename = fd.askopenfilename(
+        title='Open a file',
+        initialdir='/',
+        filetypes=filetypes)
+
+    showinfo(
+        title='Selected File',
+        message=filename
+    )
 
 root = Tk()
+root.title('Parser Wix')
+root.geometry('320x240')
+
+
 frm =ttk.Frame(root,padding=10)
 frm.grid()
 ttk.Label(frm, text="Hello world!").grid(column=0,row=0)
-ttk.Button(frm, text="Quit", command = root.destroy).grid(column=1,row=0)
+ttk.Label(frm, text="column1row0").grid(column=1,row=0)
+
+ttk.Button(frm, text="Quit", command = root.destroy).grid(column=1,row=2)
 
 ttk.Button(frm, text="Scan(rescan)").grid(column=0,row=2)
+
+open_button = ttk.Button(
+    frm,
+    text='Open a File',
+    command=select_file
+).grid(column=0,row=3)
+##open_button.pack(expand=True)
+##tkinter.filedialog.askopenfile(mode='r', **options)
 
 root.mainloop()
 
